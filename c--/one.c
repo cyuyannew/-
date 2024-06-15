090d0bdf8be496e4ba0ca1621e72ada12f342062
yu lingxin <113274474+cyuyannew@users.noreply.github.com>
2024-06-15 02:59:16 +0000

    1

--- c--/one.c
+++ c--/one.c
@@ -1,6 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    printf("hehe\n");
+    printf("%d\n",sizeof(char));
+    printf("%zu\n",sizeof(long));
+    printf("%d\n",sizeof(short));
+    printf("%d\n",sizeof(long long));
     return 0;
 }
+
+// 计算机中的单位
+// bit 比特位
+// byte 字节     1字节 = 8bit
+// kb
+// mb
+// tb
+// pb
