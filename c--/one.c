aec49b39efad824f426768da914a10ce605a635e
“cyuyannew” <703752023@qq.com>
2024-06-15 13:32:20 +0800

    提交

--- c--/one.c
+++ c--/one.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-int main()
-{
-    printf("%d\n",sizeof(char));
-    printf("%zu\n",sizeof(long));
-    printf("%d\n",sizeof(short));
-    printf("%d\n",sizeof(long long));
-    return 0;
-}
+// int main()
+// {
+//     printf("%d\n",sizeof(char));
+//     printf("%zu\n",sizeof(long));
+//     printf("%d\n",sizeof(short));
+//     printf("%d\n",sizeof(long long));
+//     return 0;
+// }
 
 // 计算机中的单位
 // bit 比特位
@@ -15,4 +15,43 @@ int main()
 // mb
 // tb
 // pb
-/////
+
+
+// int main()
+// {
+//     int age = 20;
+//     double price 66.6;
+//     return 0;
+// }
+
+
+// 变量、常量  分为局部变量和局部变量
+
+
+// int main()
+// {
+
+//     return 0;
+// }
+
+// int a = 100;  全局变量
+// int main ()
+// {
+//     int a = 10;  局部变量。局部变量优先
+//     printf("a=%d\n",a);
+//     return 0;
+// }
+
+// 写一个代码计算两个整数的和
+
+int main()
+{
+    int num1 = 0;
+    int num2 = 0; //初始化
+    // 输入两个整数
+    scanf ("%d %d",&num1,&num2);  //scanf是输入
+    // 求和
+    int sum = num1 + num2;
+    printf("%d\n",sum);
+    return 0;
+}
