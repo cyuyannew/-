61dec1baca3a70a473dae72b5dc0d39e1a4b1ad4
“cyuyannew” <703752023@qq.com>
2024-06-16 14:49:43 +0800

    提交

--- c--/one.cpp
+++ c--/one.cpp
@@ -1,5 +1,5 @@
 #include<stdio.h>
-
+//1.字面常量
 //    30；就是这个字面常量
 //    3.14；字面浮点型的常量
 //    'w';字符常量
@@ -10,8 +10,10 @@
 //1.字面常量
 //2.const修饰的常变量
 //3.define定义的标识符常量
+//4.枚举常量
 
 
+//2.const 修饰的常变量
 //int main()
 //{
 ////    const int a = 10;  //const修饰的常变量,不能被改变。在c语言中，const修饰的a，本质是变量
@@ -24,14 +26,36 @@
 //    return 0;
 //}
 ////理解：变量是个空间，常量是个实际的值，变量用来容纳常量，const只是锁定了值，但并不是把空间变成数值
+
+//3.define定义的标识符常量
 //%s 是用来打印字符串的 比如  printf("%s\n",某)
-#define MAX 100 //MAX是一个标识符
-#define APP "abcs"
-int main ()
+//#define MAX 100 //MAX是一个标识符
+//#define APP "abcs"
+//int main ()
+//{
+//    printf("%d\n",MAX);
+//    int a = MAX;
+//    printf("%d\n",a);
+//    printf("%s\n",APP);
+//    return 0;
+//}
+
+//4.枚举常量
+//enum枚举的关键字
+enum Color
 {
-    printf("%d\n",MAX);
-    int a = MAX;
-    printf("%d\n",a);
-    printf("%s\n",APP);
+    //这三个可能的取值就是枚举常量
+    RED,
+    BLUE,
+    GREEN
+};
+const char* color_names[] = {"RED","BLUE","GREEN"};
+int main()
+{
+    //int num = 10;
+    enum Color color = RED;
+    enum Color MyCOLOR = BLUE;
+    printf("color = %s\n",color_names[color]);
+    printf("MyCOLOR = %s\n",color_names[MyCOLOR]);
     return 0;
 }
